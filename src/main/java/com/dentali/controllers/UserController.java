6f87647036c961ed48a70e93405bb4e73415ff9d
Shomou <edmundo.jayr.perez.rosales@gmail.com>
2025-06-30 23:30:15 -0600

    Se anexa la semi-implementación del JWT ya desde la autenticacion del usuario queda pendiente la generación dinamica del token

--- src/main/java/com/dentali/controllers/UserController.java
+++ src/main/java/com/dentali/controllers/UserController.java
@@ -1,15 +1,12 @@
 package com.dentali.controllers;
 
-//import java.util.HashMap;
+
 import java.util.List;
-//import java.util.Map;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
